8d838c8b0a8afdfa5c0ea4814159040ca214faac
Jonathan Mora <nathanmorax@outlook.com>
2025-09-23 13:11:32 -0600

    Initial commit

---
+++ CombinedWeatheriOS-MacOSApp/CombinedWeatheriOS_MacOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombinedWeatheriOS_MacOSAppApp.swift
+//  CombinedWeatheriOS-MacOSApp
+//
+//  Created by Jonathan Mora on 23/09/25.
+//
+
+import SwiftUI
+
+@main
+struct CombinedWeatheriOS_MacOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
